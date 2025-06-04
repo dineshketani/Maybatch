c97b8c46a008d21fa200c74861c34b72231f6bec
dineshketani <dineshketani.srinivas@gmail.com>
2025-06-04 19:44:48 +0530

    updated

---
+++ aem-may-batch/core/src/main/java/com/adobe/aem/may/batch/core/servlets/DemoServlet.java
@@ -0,0 +1,27 @@
+package com.adobe.aem.may.batch.core.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletPaths;
+import org.osgi.service.component.annotations.Component;
+
+@Component(service=Servlet.class)
+@SlingServletPaths( value = "/bin/may/sample")
+public class DemoServlet extends SlingSafeMethodsServlet{
+
+    @Override
+    protected void doGet(final SlingHttpServletRequest req,
+            final SlingHttpServletResponse resp) throws ServletException, IOException {
+        final Resource resource = req.getResource();
+        resp.setContentType("text/plain");
+        resp.getWriter().write("Title = " + "Dinesh");
+    }
+
+}
